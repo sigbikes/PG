e9d70cf33588b24ace700c5fe1acc5228691e255
sigbikes <sigbikes@gmail.com>
2018-01-23 21:43:17 -0600

    New Repo

---
+++ wrd_postgis_connect.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 16 10:50:07 2018
+
+@author: Sig
+"""
+
+
+
+import psycopg2
+import os, sys
+import pandas as pd
+from datetime import datetime
+
+try:
+    conn = psycopg2.connect("dbname='wrd_postgis' user='postgres' host=192.168.60.11 password='L41Pu]E{Uqk4YP%N=f0-%psT6BIAw:'")
+except:
+    print "I am unable to connect to the database"
+
+cur = conn.cursor()
+
+cur.execute('select id, unitname, formation, operator, county, acreage, wrd_wi, wrd_nri from "BurlesonUnitPlan"')
+
+rows = cur.fetchall()
+
+output = pd.DataFrame(list(rows))
+head = ['id', 'unitname', 'formation', 'operator', 'county', 'acreage', 'wrd_wi', 'wrd_nri']
+output.columns = head
+
+''' *****************Close Up Shop***************** '''
+cur.close()
+conn.close()
+
+del rows
+
+dt = str(datetime.now())
+dt= dt.replace('-', '')
+
+output.to_csv('/Users/Sig/Python/Rigs/BurlesonUnitPlan_'+dt[:8]+'.csv', index = 0)
